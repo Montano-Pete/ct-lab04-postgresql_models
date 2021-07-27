a63cbfa7ab7f7f30de56559609086bcf0e71a6d5
Peter <pete225848@gmail.com>
2021-07-27 00:04:14 -0700

    passing tets for DELETE route (refactor)

--- __tests__/album.test.js
+++ __tests__/album.test.js
@@ -77,13 +77,15 @@ describe('album routes', () => {
     });
   });
 
-  it('delets an album by id via DELETE', async () => {
+  // TEST BELOW CURRENTLY DOES NOT PASS
+
+  it('deletes an album by id via DELETE', async () => {
     const roomOnFire = await Album.insert({
       name: 'Room On Fire',
       artist: 'The Strokes',
       yearReleased: 2003
     });
-
+    
     const res = await request(app)
       .delete(`/api/v1/albums/${roomOnFire.id}`);
 
