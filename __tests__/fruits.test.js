b00629855ee0db906bac4627e9d9d2868c052aef
Peter <pete225848@gmail.com>
2021-07-27 00:58:58 -0700

    failing PPOST test for fruits route (red)

---
+++ __tests__/fruits.test.js
@@ -0,0 +1,23 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Fruit from '../lib/models/Fruit.js';
+
+describe('fruit routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a fruit VIA post', async () => {
+    const fruit = { name: 'red apple', sweet: true, treeFruit: true };
+    const res = await request(app)
+      .post('/api/v1/fruits')
+      .send(fruit);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...fruit
+    });
+  });
+});
