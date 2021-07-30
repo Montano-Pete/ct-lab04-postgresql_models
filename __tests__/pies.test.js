5475f73c492a70a61bb6075b7c9fd7e11b83ea69
Peter <pete225848@gmail.com>
2021-07-30 11:59:37 -0700

    file prep for pies table - created model, controller, test, table

---
+++ __tests__/pies.test.js
@@ -0,0 +1,11 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Pie from '../lib/models/Pie.js';
+
+describe('pie routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+});
