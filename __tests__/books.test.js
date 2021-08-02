39e63c4e67f75884a95a23ecbf1004269c3eedc6
Peter <pete225848@gmail.com>
2021-08-02 11:11:55 -0700

    failing POST test for books route (red)

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -8,4 +8,23 @@ describe('book routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('creates a book VIA post', async () => {
+    const book = {
+      title: 'The Alchemist',
+      author: 'Paulo Coelho',
+      genre: 'Fantasy',
+      isbn10: 9780061122415,
+      pageCount: 197
+    };
+
+    const res = await request(app)
+      .post('/api/v1/books')
+      .send(book);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...book
+    });
+  });
 });
