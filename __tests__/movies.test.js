b9e40e48097234cb3357e0baffcb714ace4cf4d2
Peter <pete225848@gmail.com>
2021-08-02 10:04:57 -0700

    failing GET all test for movies route (red)

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -39,4 +39,35 @@ describe('movie routes', () => {
 
     expect(res.body).toEqual(movie);
   });
+
+  it('gets all movies via GET', async () => {
+    const darkKnight = {
+      title: 'The Dark Knight',
+      director: 'Christopher Nolan',
+      yearReleased: 2008,
+      domesticBoxOffice: '$534,858,444'
+    };
+    const shawshank = await Movie.insert({
+      title: 'The Shawshank Redemption',
+      director: 'Frank Darabont',
+      yearReleased: 1994,
+      domesticBoxOffice: '$28,699,976'
+    });
+    const jurassicPark = await Movie.insert({
+      title: 'Jurassic Park',
+      director: 'Steven Spielberg',
+      yearReleased: 1993,
+      domesticBoxOffice: '$404,214,720'
+    });
+    const bladeRunner = await Movie.insert({
+      title: 'Blade Runner',
+      director: 'Ridley Scott',
+      yearReleased: 1982,
+      domesticBoxOffice: '$32,868,943'
+    });
+
+    const res = await request(app).get('/api/v1/movies');
+
+    expect(res.body).toEqual([darkKnight, shawshank, jurassicPark, bladeRunner]);
+  });
 });
